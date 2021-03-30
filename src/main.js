e7da9992f28ab2e392b882c25c0d8fb73d993dd9
isabelajs <isabelajimenezs96@gmail.com>
2021-03-30 13:11:28 -0500

    header and search

--- src/main.js
+++ src/main.js
@@ -5,3 +5,19 @@ import {router} from "./router/indexRoutes.js"
 window.addEventListener("hashchange", ()=>{
    router(window.location.hash)
 })
+
+window.onresize = pepe;
+window.onload = pepe;
+
+function pepe(){
+   let iconMenu = document.querySelector(".header__icon-menu")
+   let menu = document.querySelector(".header__menu")
+
+   if (screen.width >= 768){     
+      iconMenu.classList.add("hidden")
+      menu.classList.remove("hidden")
+   }else{
+      iconMenu.classList.remove("hidden")
+      menu.classList.add("hidden")
+   }
+}
