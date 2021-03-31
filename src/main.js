f9cec76a7d3bccad11f16fa59470888457b3bd63
isabelajs <isabelajimenezs96@gmail.com>
2021-03-30 23:31:56 -0500

    mejore el header, barra de opciones con funciones js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 import "./styles/style.scss"
-
 import {router} from "./router/indexRoutes.js"
+
 
 window.addEventListener("hashchange", ()=>{
    router(window.location.hash)
 })
 
-window.onresize = pepe;
-window.onload = pepe;
+//cambio de elementos del header
+window.addEventListener("resize",()=>{hiddenElementsHeader(),moveSearch()});
+window.addEventListener("load",()=>{hiddenElementsHeader(),moveSearch()});
 
-function pepe(){
+function hiddenElementsHeader(){
    let iconMenu = document.querySelector(".header__icon-menu")
    let menu = document.querySelector(".header__menu")
 
@@ -21,3 +22,36 @@ function pepe(){
       menu.classList.add("hidden")
    }
 }
+
+function moveSearch(){
+   let search = document.querySelector(".cSearch")
+   if(screen.width>=768){
+      let optionsBar = document.querySelector(".cOptionsBar")
+      optionsBar.insertAdjacentElement("afterbegin",search)
+   } else{
+      let app = document.querySelector(".app")
+      app.insertAdjacentElement("afterbegin",search)
+   }
+   
+}
+
+//cambio del componente optionbutton
+let optionButtons =  document.querySelectorAll(".cOptionButton")
+optionButtons.forEach(button =>{button.addEventListener("click", (x)=>buttonAppearanceChange(x.path[0]))})
+
+
+function buttonAppearanceChange(button){
+   button.classList.toggle("cOptionButton--selected")
+}
+
+//cambio la selección en los estados de vida
+let containerLife = document.querySelectorAll(".containerLife")
+containerLife.forEach(container =>{container.addEventListener("click", (x)=>{selectStatusLife(x.path[0])})})
+
+function selectStatusLife(container){
+   container.classList.toggle("hideContainerLife")
+}
+
+
+
+
