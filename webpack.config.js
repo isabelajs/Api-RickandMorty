a831f4ffcf46e5ce1977fd27f1469216adcbb305
isabelajs <isabelajimenezs96@gmail.com>
2021-05-03 21:49:30 -0500

    cambio en el nombre de la carpeta dist

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports= {
    entry: "./src/main.js",
 
    output: {
-      path: path.resolve(__dirname,"dist"),
+      path: path.resolve(__dirname,"docs"),
       filename: "main.js"
    },
 
