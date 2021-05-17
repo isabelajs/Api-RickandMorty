109772cd1a949f2fc5a431c5c2e3ec4fc80759fb
isabelajs <isabelajimenezs96@gmail.com>
2021-05-17 01:00:31 -0500

    quite la función click del search

--- src/views/home/cCharacters/cCharacters.js
+++ src/views/home/cCharacters/cCharacters.js
@@ -21,29 +21,39 @@ let cCharacters = ()=>{
 //renderiza los personajes
 async function renderCharacters(){
   let data = await dataCharacters.filtrar()
-  //modifica en el dataPage el número de paginas a renderizar
-  dataPage.numberOfPages = Math.ceil(data.length/dataPage.numberCharactersPage)
-  
   let characters =  document.querySelector(".cCharacters__characters")
   characters.innerHTML = ""
   
-  //evalua si es necesario crear más paginas según el número de personajes
-  if(data.length > dataPage.numberCharactersPage){
+  
+  if(data.length !== 0){
+    //modifica en el dataPage el número de paginas a renderizar
+    dataPage.numberOfPages = Math.ceil(data.length/dataPage.numberCharactersPage)
+    
 
-    let groupCharacters = data.slice(dataPage.numberCharactersPage*(dataPage.pageNumber-1),dataPage.numberCharactersPage*dataPage.pageNumber)
-    groupCharacters.forEach(character => {
-      characters.appendChild(cCharacterCard(character))
-    });
-    renderPageNumbering()
+      //evalua si es necesario crear más paginas según el número de personajes
+    if(data.length > dataPage.numberCharactersPage){
+
+      let groupCharacters = data.slice(dataPage.numberCharactersPage*(dataPage.pageNumber-1),dataPage.numberCharactersPage*dataPage.pageNumber)
+      groupCharacters.forEach(character => {
+        characters.appendChild(cCharacterCard(character))
+      });
+      renderPageNumbering()
+
+    }else{
+      data.forEach(character => {
+        characters.appendChild(cCharacterCard(character))
+      });
+      //renderiza la numeración de la página
+      let pageNumbering = document.querySelector(".pageNumbering")
+      pageNumbering. innerHTML = "<div class='pageNumbering__index'> <div class='indexNumber indexNumber__selected'>1</div> </div>"
+    }
 
   }else{
-    data.forEach(character => {
-      characters.appendChild(cCharacterCard(character))
-    });
-    //renderiza la numeración de la página
-    let pageNumbering = document.querySelector(".pageNumbering")
-    pageNumbering. innerHTML = "<div class='pageNumbering__index'> <div class='indexNumber indexNumber__selected'>1</div> </div>"
+    let charactersNotFound = document.createElement("div")
+    charactersNotFound.classList.add("charactersNotFound")
+    characters.appendChild(charactersNotFound)
   }
+
 
 }
 
