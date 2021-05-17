bac942136c4e488db799b830bda5ca978d8620dd
isabelajs <isabelajimenezs96@gmail.com>
2021-05-17 16:07:08 -0500

    quite la función mover buscador

--- src/views/home/cSearch.js
+++ src/views/home/cSearch.js
@@ -3,7 +3,7 @@ import { renderCharacters } from "./cCharacters/cCharacters.js"
 
 let cSearch = ()=>{
       let view = `      <button class="cSearch__buttonSend"></button>
-                        <input class="cSearch__search"  id ="buscador" placeholder="Buscar">
+                        <input class="cSearch__search" type="text"  id ="buscador" placeholder="Buscar">
                         <input type="text">`
       let search = document.createElement("div")
       search.classList.add("cSearch")
--- src/views/home/home.js
+++ src/views/home/home.js
@@ -10,10 +10,10 @@ let main = ()=>{
    cMain.appendChild(cOptionsBar())
    cMain.appendChild(cCharacters())
 
-   //cambio de posicion en los elementos del header
-   window.addEventListener("resize",()=>{moveSearch()});
-   window.addEventListener("load",()=>{moveSearch()});
- 
+   // //cambio de posicion en los elementos del header
+   // window.addEventListener("resize",()=>{moveSearch()});
+   // window.addEventListener("load",()=>{moveSearch()});
+
    
    return cMain
 
